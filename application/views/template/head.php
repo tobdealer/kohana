ce06dc0a4297397579e6c55b50f3e9e82d18909f
Karl Sommer <sommer@gymnasium-altluenen.de>
2014-12-31 12:08:03 +0100

    Bugfix in using Session

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -5,7 +5,18 @@
  <div style="margin:0px auto;"  align="center">
  <h1>Welcome</h1>
  </div>
- <?php if(!$_SESSION['logged_in']):?>
+ <?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']):?>
+        <form action="logout" id="frm" method="post">
+</form>
+<table class="button">
+        <tr><td onclick="document.getElementById('frm').submit();">
+        
+  <div class="b1 b11"></div>
+  <div class="b1 b12">Ausloggen</div>
+  <div class="b1 b13"></div>
+        </td>
+        </tr></table>
+        <?php else:?>
 <form action="login" id="frm" method="post">
 Username:<br>
 <input type="text" name="username"></input><br>
@@ -17,17 +28,6 @@ Password:<br>
         
   <div class="b1 b11"></div>
   <div class="b1 b12">Einloggen</div>
-  <div class="b1 b13"></div>
-        </td>
-        </tr></table>
-        <?php else:?>
-        <form action="logout" id="frm" method="post">
-</form>
-<table class="button">
-        <tr><td onclick="document.getElementById('frm').submit();">
-        
-  <div class="b1 b11"></div>
-  <div class="b1 b12">Ausloggen</div>
   <div class="b1 b13"></div>
         </td>
         </tr></table>
